fb8d7acd5c5cd34734ea6909d2b114b44f3ddde6
Justin McNary <justinmcnary@McNary-Macbook.local>
2017-03-15 15:54:51 -0400

    Dual function and minor bug fix

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
 const appID = '5ff9d9d1';
 const appKey = '8a580ee40ddee9e24cc9c02664aced07';
 
-function getFood(query){
+const state = {
+  searchItems: [],
+  storedItems: []
+}
+
+
+function getFoodFromApi(query){
   $.getJSON(`https://api.nutritionix.com/v1_1/search/${query}?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=5ff9d9d1&appKey=8a580ee40ddee9e24cc9c02664aced07`, (d)=>{
-    console.log(d);
-    let result = {
-      Food: d.hits["0"].fields.item_name,
-      Calories: d.hits["0"].fields.nf_calories
-    };
-    console.log(result);
+    state.searchItems = d.hits;
+    displayResults();
   });
+
 };
+
+const addItem = function(state, item) {
+  console.log(state);
+  state.storedItems.push(item);
+}
+
+const removeItem = function(item) {
+  console.log(state.storedItems.indexOf(item));
+  state.storedItems.splice(state.storedItems.indexOf(item), 1);
+}
+
+const displayTodaysMenu = function() {
+  $('.js-todays-meals').html("");
+  state.storedItems.map(function(item) {
+  $('.js-todays-meals').append(
+    `<div class= "mealBar">
+     <p>${item}</p>
+     <button class= "delete"> Delete </button>
+     </div>`)
+  });
+}
+
+const displayResults = function() {
+  $('.js-search-results').html("");
+  state.searchItems.map(function(item) {
+  let items = item.fields;
+  $('.js-search-results').append(
+    `<div class= "itemBar">
+     <h2> ${items.brand_name} ${items.item_name} </h2>
+     <p> Calories: ${items.nf_calories} </p>
+     <p> Serving Size: ${items.nf_serving_size_qty} ${items.nf_serving_size_unit} </p>
+     <p> Total Fat: ${items.nf_total_fat} </p>
+     <button class= "select"> Select </button>
+     </div>`)
+  });
+}
 
 
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     let query = $(this).find('.js-query').val();
-    console.log(query);
-    getFood(query);
+    getFoodFromApi(query);
   });
 }
 
+$('.flexcontainer').on('click','.select', function(){
+  addItem(state, $(this).closest('.itemBar').text());
+  displayTodaysMenu();
+});
+
+$('div').on('click', 'button.delete', function(event){
+  removeItem($(this).closest('div').text());
+  displayTodaysMenu();
+});
+
 watchSubmit();
-
-
-
-
-// function eventListner() {
-//   $('#search-bar').submit(function(e){
-//     e.preventDefault();
-//     searchValue();  
-//   })
-// }
-
-// eventListner();
-// function getDataFromApi(searchTerm, callback) {
-//   var query = {
-//     "appId": appID,
-//     "appKey": appKey,
-//     "query": "Cookies `n Cream"
-//   }
-//   $.getJSON(url, query, callback);
-// }
-
-// getDataFromApi();
